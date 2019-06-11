acfd0666cf1dcb94d6a67704b706d8a86d6539bd
alexis1409 <51690254+alexis1409@users.noreply.github.com>
2019-06-10 23:31:22 -0500

    Add files via upload

---
+++ alumno/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Lectura</title>
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/simple-sidebar.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading"> <img src="../img/user (2).png" class="user" alt=""> <label for="">claudio <br> 1° "A"</label> </div>
+      <div class="list-group list-group-flush">
+      <a href="index.php" class="list-group-item list-group-item-action bg-light meni"> <img src="../img/open-book.png"  alt=""> Lecturas</a>
+        <a href="act.php" class="list-group-item list-group-item-action bg-light meni"> <img src="../img/jigsaw.png" alt=""> Actividades</a>
+        <a href="cali.php" class="list-group-item list-group-item-action bg-light meni"> <img src="../img/evaluation.png" alt=""> Calificaciones</a>
+        <a href="config.php" class="list-group-item list-group-item-action bg-light meni"> <img src="../img/wrench.png" alt=""> Configuracion</a>
+       
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+    
+      <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+
+        <button class="navbar-toggler-icon bn"  id="menu-toggle"> </button>
+
+      </nav>
+
+      <div class="container-fluid">
+       
+      <h1 class="mt-2"><img src="../img/open-book.png"  alt="" style="height:40px">Lecturas</h1>
+     
+        <div id="" class="container">
+                        
+                        <div class="row mt-4">
+
+                            <div class="col-6 col-sm-4 col-md-3 col-lg-2 card">
+                                <a href="#" data-toggle="modal" data-target="#imagen1">
+                                    <img src="../img/lectura1.jpg" class="img-fluid mb-4">
+                                </a> 
+                                <h3>La rata</h3>
+                            </div>
+
+                            <div class="col-6 col-sm-4 col-md-3 col-lg-2 card">
+                                <a href="#" data-toggle="modal" data-target="#imagen1">
+                                    <img src="../img/lectura1.jpg" class="img-fluid mb-4">
+                                </a> 
+                                <h3>La rata</h3>
+                            </div>
+
+                            
+</div>
+</div>
+
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Menu Toggle Script -->
+  <script>
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  </script>
+
+</body>
+
+</html>
